0ebdf908cb4ea4faf0ed73ebb02324fa40a278d3
Martina Ricci <martinaricci@Martinas-MacBook-Air.local>
2018-02-05 01:01:51 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 console.log("Up and running!");
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king'
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cards[0] = "queen";
+
+cardsInPlay.push(cards[0]);
+console.log("User flipped queen");
+
+var cards[3] = "king";
+
+cardsInPlay.push(cards[3]);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match");
+	}
+	else {
+		alert("sorry try again")
+	}
+}
