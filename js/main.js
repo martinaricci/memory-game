9ed84352dcde2f84995dcec4fba65b2598665127
Martina Ricci <martinaricci@Martinas-MacBook-Air.local>
2018-02-12 23:41:30 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -33,12 +33,14 @@ console.log("Sorry, try again.");
 }
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id");	
 	console.log("User flipped" + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
 
+this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[2]) {
 		alert("you found a match");
@@ -49,5 +51,14 @@ if (cardsInPlay.length === 2) {
 }
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
